71292d70e24d2647be62f3a256f8f697990e24b0
waltersmatthew <waltersmatthew88@gmail.com>
2022-11-10 17:05:24 -0800

    chore: add home routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom' 
+import Home from './pages/Home';
 import Products from './pages/Products'
 import Product from './pages/Product';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route
+          path=''
+          element={<Home />}
+        />
         <Route
           path='/products'
           element={<Products />}
