be3819b6eeedbf15bb045c079abda108eab6c8b2
waltersmatthew <waltersmatthew88@gmail.com>
2022-12-12 13:40:37 -0800

    remove unused imports

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,7 @@
-import { useParams, useLocation } from "react-router-dom"
+import { useLocation } from "react-router-dom"
 
 export default function Product(){
 
-    const { id } = useParams()
     const location = useLocation()
     const prod = location.state   
     
